2647d10957cc5ad965fd3f2d9429d4dac2401f5a
Scav <42140533+Scavix@users.noreply.github.com>
2023-09-18 04:23:33 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [inputData, setinputData] = React.useState("Hello world!");
+  const [receivedData, setreceivedData] = React.useState(null);
   const url = "https://morse-prolog.onrender.com/";
 
-  React.useEffect(() => {
-    fetch(url+"api")
+  const handleInputChange = (e) => {
+    setinputData(e.target.value);
+  };
+
+  const sendToServerFromMorse = () => {
+    fetch(url + "apiFromMorse", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ sentence: inputData }),
+    })
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data) => setreceivedData(data.message));
+  };
+
+  const sendToServerToMorse = () => {
+    fetch(url + "apiToMorse", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ sentence: inputData }),
+    })
+      .then((res) => res.json())
+      .then((data) => setreceivedData(data.message));
+  };
+
+  React.useEffect(() => {
+    fetch(url + "hello")
+      .then((res) => res.json())
+      .then((data) => setreceivedData(data.message));
   }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+      <h1>Node and React Prolog Morse code transliterator</h1>
+      <div className="translator">
+        <input
+          type="text"
+          placeholder="Enter a sentence to translate morse"
+          value={inputData}
+          onChange={handleInputChange}
+        />
+        <div className="buttons">
+          <button
+            onClick={sendToServerFromMorse()}
+          > Morse to Text</button>
+          <button
+            onClick={sendToServerToMorse()}
+          >Text to Morse</button>
+        </div>
+        <p>{!receivedData ? "Loading..." : receivedData}</p>
+      </div>
     </div>
   );
 }
